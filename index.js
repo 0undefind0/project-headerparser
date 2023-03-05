b6e6f169770d2b496f355ac6c70c61ced22e04c9
martin <steam.martinjuan@gmail.com>
2023-03-05 22:43:05 +0800

    added redundancy for getting ip

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/api/hello', function (req, res) {
     }
 */
 app.get('/api/whoami', function (req, res) {
-  const ip = req.socket.remoteAddress;
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   const language = req.headers['accept-language'];
   const software = req.headers['user-agent'];
 
